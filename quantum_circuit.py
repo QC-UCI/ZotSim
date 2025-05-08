d265ffb46da7977cbdb1bb209be7265006eeb235
Diptanshu Sikdar <dssikdar@gmail.com>
2025-05-07 18:52:59 -0700

    added gates

--- quantum_circuit.py
+++ quantum_circuit.py
@@ -17,7 +17,7 @@ class BasicSimulator:
     def run(self, shots: int = 1024):
         pass
 
-    def _apply_full(self, unitary: np.array):
+    def _apply_full(self, unitary: np.ndarray):
         assert len(unitary.shape) == 2
         assert 2 ** self.qreg.qubits == unitary.shape[0]
         assert 2 ** self.qreg.qubits == unitary.shape[1]
@@ -37,12 +37,11 @@ class QuantumCircuit:
         self.qreg = qreg
         self.creg = creg
 
-        self.gates = []
+        self.gates: list[Gate] = []
 
 
     def apply(self, gate: Gate):
         pass
-
     
 
 
@@ -59,3 +58,27 @@ class ClassicalRegister:
         self.bits = bits
 
 
+class Gate:
+    def __init__(self, qubits: int, amplitudes: np.ndarray):
+        assert len(amplitudes.shape) == 2, "Amplitude array should be 2 dimensional"
+        assert amplitudes.shape[0] == 2 ** qubits
+        assert amplitudes.shape[1] == 2 ** qubits
+        # TODO: make sure unitary
+        self.qubits = qubits
+        self.amplitudes = amplitudes
+
+    def i():
+        id_gate = Gate(1, np.array([[1., 0.], [0., 1.]]))
+        return id_gate
+
+    def x():
+        x_gate = Gate(1, np.array([[0., 1.], [1., 0.]]))
+        return x_gate
+    
+    def z():
+        z_gate = Gate(1, np.array([[1., 0.], [0., -1.]]))
+        return z_gate
+    
+    def h():
+        h_gate = Gate(1, (1 / np.sqrt(2)) * np.array([[1., 1.], [1., -1.]]))
+        return h_gate
